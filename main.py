41f185338a89aea297def6978cb06be3e00918d5
Matteo Bortolotto <yamatteo@gmail.com>
2023-07-23 15:35:38 +0200

    Testing curent directory

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import Request
 from fastapi.responses import HTMLResponse
 import uvicorn
+from pathlib import Path
 
 
 from backend.main import app
 
 @app.get("/")
 async def get_index(request: Request):
+    return HTMLResponse(content=str(list(Path(".").iterdir())), status_code=200)
     with open("./frontend/index.html", "r") as f:
         return HTMLResponse(content=f.read(), status_code=200)
 
